32965e98b88f3abe7fe927e37da636786dde257d
Marat Tazhetdinov <tms1703@gmail.com>
2024-02-24 17:48:25 -0800

    DONE add dialog loader

--- lib/ui_kit/loaders/app_loader.dart
+++ lib/ui_kit/loaders/app_loader.dart
@@ -1,5 +1,13 @@
 part of '../ui_kit_part.dart';
 
+void showLoader(BuildContext context) {
+  showDialog(
+      context: context,
+      builder: (context) {
+        return const AppLoader();
+      });
+}
+
 class AppLoader extends StatefulWidget {
   const AppLoader({
     super.key,
@@ -10,25 +18,61 @@ class AppLoader extends StatefulWidget {
 }
 
 class _AppLoaderState extends State<AppLoader> with TickerProviderStateMixin {
-  late final AnimationController _controller;
-  late final Animation _animation;
-  // late final Animation _secondAnimation;
-  // late final Animation _thirdAnimation;
+  late final AnimationController _cannonballAnimationController;
+  late final Animation _cannonballAnimation;
+  late final AnimationController _shipAnimationController;
+  late final Animation _shipAnimation;
 
   @override
   void initState() {
-    _controller = AnimationController(
+    _cannonballAnimationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 1300),
+      duration: const Duration(
+          milliseconds: defaultLoadingAnimationDurationInMilliseconds),
     );
-    _animation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
-      parent: _controller,
+    _cannonballAnimation =
+        Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
+      parent: _cannonballAnimationController,
       curve: Curves.easeInOut,
     ));
-    _controller.forward();
-    _controller.addStatusListener((status) {
+    _cannonballAnimationController.forward();
+    _cannonballAnimationController.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
-        _controller.forward(from: 0);
+        _cannonballAnimationController.forward(from: 0);
+      }
+    });
+    _shipAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+          milliseconds: defaultLoadingAnimationDurationInMilliseconds),
+    );
+    _shipAnimation = TweenSequence<double>(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(
+            begin: -0.05,
+            end: 0.05,
+          ),
+          weight: 50,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(
+            begin: 0.05,
+            end: -0.05,
+          ),
+          weight: 50,
+        ),
+      ],
+    ).animate(
+      CurvedAnimation(
+        parent: _shipAnimationController,
+        curve: Curves.linear,
+      ),
+    );
+    _shipAnimationController.forward();
+    _shipAnimationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _shipAnimationController.forward(from: 0);
       }
     });
     super.initState();
@@ -36,8 +80,11 @@ class _AppLoaderState extends State<AppLoader> with TickerProviderStateMixin {
 
   @override
   void dispose() {
-    _controller
+    _cannonballAnimationController
       ..removeStatusListener((_) {})
+      ..dispose();
+    _shipAnimationController
+      ..removeStatusListener((status) {})
       ..dispose();
     super.dispose();
   }
@@ -63,77 +110,129 @@ class _AppLoaderState extends State<AppLoader> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    final colors = context.theme.colors;
-    return SizedBox(
-      height: 100,
-      width: double.infinity,
-      child: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          LayoutBuilder(
-            builder: (context, constraints) {
-              final size =
-                  Size(constraints.maxWidth - 180, constraints.maxHeight);
-              final pathesList = [
-                _drawPath(
-                  size.height,
-                  size,
-                ),
-                _drawPath(
-                  size.height / 1.3,
-                  size,
-                ),
-                _drawPath(
-                  size.height / 1.8,
-                  size,
-                ),
-              ];
-              return SizedBox(
-                height: size.height,
-                width: size.width,
-                child: Stack(
-                  children: [
-                    ...pathesList.map(
-                      (path) => AnimatedBuilder(
-                        animation: _animation,
-                        builder: (context, child) {
-                          return Positioned(
-                            top: _calculateOffset(_animation.value, path).dy,
-                            left: _calculateOffset(_animation.value, path).dx,
-                            child: CircleAvatar(
-                              backgroundColor: colors.firstTextColor,
-                              radius: 5,
+    final theme = context.theme;
+    final colors = theme.colors;
+    final locale = context.l10n;
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            border: Border.all(
+              color: colors.firstTextColor,
+            ),
+            color: colors.scaffoldBackgroundColor,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 100,
+                  width: double.infinity,
+                  child: Stack(
+                    alignment: AlignmentDirectional.center,
+                    children: [
+                      LayoutBuilder(
+                        builder: (context, constraints) {
+                          final size = Size(constraints.maxWidth - 180,
+                              constraints.maxHeight);
+                          final pathsList = [
+                            _drawPath(
+                              size.height,
+                              size,
+                            ),
+                            _drawPath(
+                              size.height / 1.3,
+                              size,
+                            ),
+                            _drawPath(
+                              size.height / 1.8,
+                              size,
+                            ),
+                          ];
+                          return SizedBox(
+                            height: size.height,
+                            width: size.width,
+                            child: Stack(
+                              children: [
+                                ...pathsList.map(
+                                  (path) => AnimatedBuilder(
+                                    animation: _cannonballAnimation,
+                                    builder: (context, _) {
+                                      return Positioned(
+                                        top: _calculateOffset(
+                                                _cannonballAnimation.value,
+                                                path)
+                                            .dy,
+                                        left: _calculateOffset(
+                                                _cannonballAnimation.value,
+                                                path)
+                                            .dx,
+                                        child: CircleAvatar(
+                                          backgroundColor:
+                                              colors.firstTextColor,
+                                          radius: 5,
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ],
                             ),
                           );
                         },
                       ),
-                    ),
-                  ],
+                      Positioned(
+                        left: 0,
+                        child: SizedBox(
+                          width: 100,
+                          child: AnimatedBuilder(
+                              animation: _shipAnimationController,
+                              builder: (context, _) {
+                                return Transform.rotate(
+                                  angle: _shipAnimation.value,
+                                  child: Image.asset(
+                                    Assets.images.loaderShipOne.path,
+                                    color: colors.firstTextColor,
+                                  ),
+                                );
+                              }),
+                        ),
+                      ),
+                      Positioned(
+                        right: 0,
+                        child: SizedBox(
+                          width: 100,
+                          child: AnimatedBuilder(
+                              animation: _shipAnimationController,
+                              builder: (context, _) {
+                                return Transform.rotate(
+                                  angle: _shipAnimation.value,
+                                  child: Image.asset(
+                                    Assets.images.loaderShipTwo.path,
+                                    color: colors.firstTextColor,
+                                  ),
+                                );
+                              }),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              );
-            },
-          ),
-          Positioned(
-            left: 0,
-            child: SizedBox(
-              width: 100,
-              child: Image.asset(
-                Assets.images.loaderShipOne.path,
-                color: colors.firstTextColor,
-              ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    locale.loading,
+                    style: theme.textTheme.titleMedium,
+                  ),
+                )
+              ],
             ),
           ),
-          Positioned(
-            right: 0,
-            child: SizedBox(
-              width: 100,
-              child: Image.asset(
-                Assets.images.loaderShipTwo.path,
-                color: colors.firstTextColor,
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
--- lib/ui_kit/ui_kit_part.dart
+++ lib/ui_kit/ui_kit_part.dart
@@ -4,8 +4,10 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:sea_battle/code_kit/code_kit_part.dart';
 import 'package:sea_battle/gen/assets.gen.dart';
 import 'package:sea_battle/gen/fonts.gen.dart';
+import 'package:sea_battle/l10n/l10n.dart';
 
 part 'theme/theme.dart';
 part 'colors/colors.dart';
