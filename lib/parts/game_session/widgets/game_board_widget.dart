dc9f49a5d16bd5ed409ba064ab9bde300dbbce5f
Marat Tazhetdinov <tms1703@gmail.com>
2024-05-17 21:14:54 -0700

    WIP add flame animation

--- lib/parts/game_session/widgets/game_board_widget.dart
+++ lib/parts/game_session/widgets/game_board_widget.dart
@@ -52,9 +52,14 @@ class GameBoardWidget extends StatelessWidget {
               child: Container(
                 decoration: BoxDecoration(
                   color: isEnemyBoard
-                      ? Colors.transparent
+                      ? switch (cellStates[index]) {
+                          CellState.destroyed => Colors.blue,
+                          _ => Colors.transparent,
+                        }
                       : switch (cellStates[index]) {
-                          CellState.occupied => Colors.blue,
+                          CellState.occupied ||
+                          CellState.destroyed =>
+                            Colors.blue,
                           _ => Colors.transparent,
                         },
                   border: Border.all(
@@ -63,7 +68,7 @@ class GameBoardWidget extends StatelessWidget {
                   ),
                 ),
                 child: switch (cellStates[index]) {
-                  CellState.destroyed => Image.asset(Assets.images.fire.path),
+                  CellState.destroyed => const AnimatedFireWidget(),
                   CellState.shot => Image.asset(Assets.images.splash.path),
                   _ => null,
                 },
@@ -73,6 +78,62 @@ class GameBoardWidget extends StatelessWidget {
         },
         itemCount: 100,
       ),
+    );
+  }
+}
+
+class AnimatedFireWidget extends StatefulWidget {
+  const AnimatedFireWidget({super.key});
+
+  @override
+  State<AnimatedFireWidget> createState() => _AnimatedFireWidgetState();
+}
+
+class _AnimatedFireWidgetState extends State<AnimatedFireWidget>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _animation = TweenSequence<double>(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.0, end: 0.85),
+          weight: 50,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.85, end: 1),
+          weight: 50,
+        ),
+      ],
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+    _animationController.forward();
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.forward(from: 0);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, _) {
+        return Transform.scale(
+          scale: _animation.value,
+          child: Image.asset(Assets.images.fire.path),
+        );
+      },
     );
   }
 }
